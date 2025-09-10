191d911c5e30f26677e24491ebb8548050ff665b
Bernardo <107374580+Bernard2806@users.noreply.github.com>
2025-09-10 18:34:14 -0300

    feat(comision-directiva): añadir Estado y migración asociada

---
+++ Vista/Data/Enums/Personal/ComisionDirectiva/EstadoComisionDirectiva.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Data.Enums.Personal.ComisionDirectiva
+{
+    public enum EstadoComisionDirectiva
+    {
+        [Display(Name = "Activo")]
+        Activo,
+
+        [Display(Name = "Baja")]
+        Baja,
+
+        [Display(Name = "Baja por Fallecimiento")]
+        BajaPorFallecimiento
+    }
+}
