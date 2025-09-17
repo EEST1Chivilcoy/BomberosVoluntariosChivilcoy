43c0f0eb77f5594cffa2207e321f5f1cce7e362e
Zothras <Zothrrass@gmail.com>
2025-09-17 08:57:25 -0300

    Fix a licencias rapido

--- Vista/Data/Enums/TipoLicencia.cs
+++ Vista/Data/Enums/TipoLicencia.cs
@@ -3,6 +3,9 @@ namespace Vista.Data.Enums
 {
     public enum TipoLicencia
     {
+        [Display(Name = "Razones laborales")]
+        RazonesLaborales,
+
         [Display(Name = "Razones personales")]
         RazonesPersonales,
 
