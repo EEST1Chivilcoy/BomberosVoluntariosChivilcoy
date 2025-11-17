05db43d4ac70b29e310041a4995a71c4bca8ba93
Bernardo <berramuspegonzalez@gmail.com>
2025-11-17 12:50:52 -0300

    feat(firmas): crear modelos de Incidentes para Móviles-Embarcaciones y Departamentos
    Se implementaron los modelos base para representar incidentes relacionados con Móviles-Embarcaciones y Departamentos.

---
+++ Vista/Data/Models/Otros/Firmas/Componentes/MovimientoIncidente.cs
@@ -0,0 +1,15 @@
+﻿using Vista.Data.Models.Personas.Personal;
+
+namespace Vista.Data.Models.Otros.Firmas.Componentes
+{
+    public class MovimientoIncidente
+    {
+        public int Id { get; set; }
+        public int EncargadoId { get; set; }
+        public Bombero Encargado { get; set; } = null!;
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string? Descripcion { get; set; }
+        public int IncidenteId { get; set; }
+        public IncidenteBase Incidente { get; set; }
+    }
+}
---
+++ Vista/Data/Models/Otros/Firmas/IncidenteMovil.cs
@@ -0,0 +1,11 @@
+﻿using Vista.Data.Models.Vehiculos.Flota;
+
+namespace Vista.Data.Models.Otros.Firmas
+{
+
+    public class IncidenteMovil : IncidenteBase
+    {
+        public VehiculoSalida Vehiculo { get; set; } = null!;
+        public int VehiculoId { get; set; }
+    }
+}
