c7faaa51b7dfb93076a90913c84fcdcf6694fae0
Bernardo <berramuspegonzalez@gmail.com>
2025-11-21 11:43:55 -0300

    feat(socios): añadir opción de forma de pago al crear socio
    Se incorpora la opción de forma de pago al proceso de alta de socios. Esta funcionalidad es temporal y podrá ajustarse o eliminarse una vez que se implemente el sistema real de pago de socios.

---
+++ Vista/Data/Migrations/20251121144241_AniadirFormaDePagoSocioBorrarLuego.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vista.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AniadirFormaDePagoSocioBorrarLuego : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FormaPago",
+                table: "Socios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FormaPago",
+                table: "Socios");
+        }
+    }
+}
--- Vista/Data/Models/Socios/Socio.cs
+++ Vista/Data/Models/Socios/Socio.cs
@@ -57,6 +57,12 @@ namespace Vista.Data.Models.Socios
         [Required(ErrorMessage = "La frecuencia de pago es obligatoria.")]
         public FrecuenciaPago? FrecuenciaDePago { get; set; } = FrecuenciaPago.Mensual;
 
+        /// <summary>
+        /// Representa la forma de pago del socio. (Por defecto es Efectivo) (Igual es algo que se va a borrar, solo incluido para la presentacion del proyecto) (Borrar Luego)
+        /// </summary>
+        [Required(ErrorMessage = "La forma de pago es obligatoria.")]
+        public FormaDePago? FormaPago { get; set; } = FormaDePago.Efectivo;
+
         // --- Datos personales ---
 
         /// <summary>
