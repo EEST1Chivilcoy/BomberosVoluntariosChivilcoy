636e0f4edb77fbbf9169e4d444c2d139380db448
Bernardo <berramuspegonzalez@gmail.com>
2025-11-21 11:28:59 -0300

    refactor(fuerzas-intervinientes-salidas): reemplazar ViewModel por ID y deshabilitar damnificados
    Se mejora el componente de Fuerzas Intervinientes de Salidas, reemplazando la referencia a la Fuerza desde ViewModel por ID. Debido a estos cambios, se vuelve a deshabilitar temporalmente el componente de Damnificados hasta su futura adaptación.

--- Vista/Data/ViewModels/Personal/FuerzaIntervinienteViewModel.cs
+++ Vista/Data/ViewModels/Personal/FuerzaIntervinienteViewModel.cs
@@ -1,10 +1,35 @@
-﻿namespace Vista.Data.ViewModels.Personal
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Data.ViewModels.Personal
 {
     public class FuerzaIntervinienteViewModel
     {
+        /// <summary>
+        /// Identificador único de la fuerza interviniente en la salida.
+        /// </summary>
         public int Id { get; set; }
-        public string NumeroUnidad { get; set; } = null!;
-        public string EncargadoApellidoyNombre { get; set; } = null!;
-        public SimpleFuerzaViewModel FuerzaViewModel { get; set; } = null!;
+
+        /// <summary>
+        /// Número de unidad de la fuerza interviniente.
+        /// </summary>
+        [Required(ErrorMessage = "El número de unidad es obligatorio.")]
+        public string? NumeroUnidad { get; set; } = null!;
+
+        /// <summary>
+        /// Encargado de la unidad que intervino en la salida.
+        /// </summary>
+        [Required(ErrorMessage = "Debe especificar el apellido y nombre del encargado.")]
+        public string? EncargadoApellidoyNombre { get; set; } = null!;
+
+        /// <summary>
+        /// Nombre de la fuerza a la que pertenece la unidad interviniente.
+        /// </summary>
+        public string? NombreFuerza { get; set; } = null!;
+
+        /// <summary>
+        /// Fuerza a la que pertenece la unidad interviniente.
+        /// </summary>
+        [Required(ErrorMessage = "Debe seleccionar la fuerza interviniente.")]
+        public int FuerzaViewModel { get; set; }
     }
 }
