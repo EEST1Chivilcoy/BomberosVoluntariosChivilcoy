9422cc675d81ad5c25a6e4871cd90787fc8bdc3a
Bernardo <berramuspegonzalez@gmail.com>
2025-11-21 12:27:58 -0300

    refactor(database): aplicar migración EF eliminando campo localidad y ajustando coordenadas
    Se genera una nueva migración de Entity Framework para reflejar cambios en el modelo de socios:
    - Se elimina el campo de localidad
    - Se hacen obligatorios los campos de longitud y latitud

---
+++ Vista/Data/Migrations/20251121152640_AjusteToSocios.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vista.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteToSocios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Localidad",
+                table: "Socios");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitud",
+                table: "Socios",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitud",
+                table: "Socios",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitud",
+                table: "Socios",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitud",
+                table: "Socios",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Localidad",
+                table: "Socios",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
