79b0d1cbf73c070237e541048c744ed8800a8468
José Luis Orihuela Conde <orihuelaconde@gmail.com>
2025-11-21 21:44:04 -0300

    feat: Mejoras en manejo de imágenes y servicios
    Se añadió un índice único para `VehiculoId` en la entidad
    `Imagen_VehiculoSalida` para garantizar unicidad en la relación
    vehículo-imagen. Se actualizó el modelo y se creó la migración
    `20251121214322_MovilImagenFix`.
    
    Se optimizó el archivo `VehiculoEditar.razor`, mejorando la
    legibilidad, validaciones y manejo de imágenes.
    
    Se simplificaron los servicios `VehiculoSalidaService` y
    `VehiculoService`, eliminando redundancias y mejorando la lógica
    para encargados e imágenes.
    
    Se eliminaron referencias innecesarias y se ajustaron validaciones
    en el método `SubirFoto` para manejar errores y formatos.

---
+++ Vista/Data/Migrations/20251121214322_MovilImagenFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vista.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovilImagenFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Imagen_VehiculoId",
+                table: "Imagen",
+                column: "VehiculoId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Imagen_VehiculoId",
+                table: "Imagen");
+        }
+    }
+}
