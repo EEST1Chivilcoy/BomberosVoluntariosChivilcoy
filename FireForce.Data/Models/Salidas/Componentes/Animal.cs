e4b15139000388c9bcd04191275ff3d03dc9be6d
José Luis Orihuela Conde <orihuelaconde@eest1chivilcoy.edu.ar>
2026-04-15 22:50:16 -0300

    Refactoriza Animal y agrega enums de tipo y estado
    Se refactoriza la clase Animal para usar enums TipoAnimal y EstadoAnimal en lugar de strings, se añade Observaciones y TipoOtro, y se reemplaza ResposableId por DamnificadoId con navegación a Damnificado_Salida. Se crean los archivos de enumeraciones TipoAnimal y EstadoAnimal. Se eliminan decoradores y referencias innecesarias para simplificar la clase.

--- FireForce.Data/Models/Salidas/Componentes/Animal.cs
+++ FireForce.Data/Models/Salidas/Componentes/Animal.cs
@@ -1,11 +1,6 @@
 ﻿using FireForce.Data.Models.Personas;
-using System;
-using System.Collections.Generic;
+using FireForce.Shared.Enums;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace FireForce.Data.Models.Salidas.Componentes
 {
@@ -14,24 +9,30 @@ namespace FireForce.Data.Models.Salidas.Componentes
         /// <summary>
         /// Identificador único del animal afectado.
         /// </summary>
-        public int? AnimalId { get; set; }
+        public int AnimalId { get; set; }
 
         /// <summary>
         /// Especie o raza del animal.
         /// </summary>
-        [StringLength(255)]
-        public string Tipo { get; set; }
+        public TipoAnimal Tipo { get; set; }
+
+        public string? TipoOtro { get; set; }
 
         /// <summary>
         /// Estado en que se encontró al animal. (Rescatado - Herido - Muerto - Desaparecido)
         /// </summary>
-        [StringLength(255)]
-        public string Estado { get; set; }
+        public EstadoAnimal Estado { get; set; }
 
         /// <summary>
         /// Cantidad del mismo animal que se registró.
         /// </summary>
         public int Cantidad { get; set; }
+
+        /// <summary>
+        /// Observaciones adicionales sobre el animal.
+        /// </summary>
+        [StringLength(255)]
+        public string? Observaciones { get; set; }
 
         /// <summary>
         /// Booleano para saber si se conoce al responsable del animal.
@@ -41,7 +42,8 @@ namespace FireForce.Data.Models.Salidas.Componentes
         /// <summary>
         /// Identificador único del responsable del animal.
         /// </summary>
-        [ForeignKey(nameof(Persona.PersonaId))]
-        public int ResposableId { get; set; }
+        public int? DamnificadoId { get; set; }
+
+        public Damnificado_Salida? Damnificado { get; set; }
     }
 }
