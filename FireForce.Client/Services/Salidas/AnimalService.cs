5884e803b1cb1fc3d194d97f75751dcbff0825bc
Giovanni Giambroni <giovannigiambroni@gmail.com>
2026-05-04 12:12:56 -0300

    Re-implementado AnimalService

--- FireForce.Client/Services/Salidas/AnimalService.cs
+++ FireForce.Client/Services/Salidas/AnimalService.cs
@@ -1,68 +1,66 @@
-﻿//using FireForce.Data;
-//using FireForce.Data.Models.Salidas.Componentes;
-//using Microsoft.EntityFrameworkCore;
+﻿using FireForce.Data;
+using FireForce.Data.Models.Salidas.Componentes;
+using Microsoft.EntityFrameworkCore;
 
-//namespace FireForce.Client.Services.Salidas
-//{
-//    public interface IAnimalService
-//    {
-//        Task<List<AnimalSalida>> ObtenerTodosLosAnimalesAsync();
-//        Task<AnimalSalida?> ObtenerAnimalPorIdAsync(int id);
-//        Task AgregarAnimalAsync(AnimalSalida animal);
-//        Task EditarAnimalAsync(AnimalSalida animal);
-//        Task BorrarAnimalAsync(int id);
-//    }
+namespace FireForce.Client.Services.Salidas
+{
+    public interface IAnimalService
+    {
+        Task<List<AnimalSalida>> ObtenerTodosLosAnimalesAsync();
+        Task<AnimalSalida?> ObtenerAnimalPorIdAsync(int id);
+        Task AgregarAnimalAsync(AnimalSalida animal);
+        Task EditarAnimalAsync(AnimalSalida animal);
+        Task BorrarAnimalAsync(int id);
+    }
 
-//    class AnimalService : IAnimalService
-//    {
-//        private readonly BomberosDbContext _context;
+    class AnimalService : IAnimalService
+    {
+        private readonly BomberosDbContext _context;
 
-//        public AnimalService(BomberosDbContext context)
-//        {
-//            _context = context;
-//        }
+        public AnimalService(BomberosDbContext context)
+        {
+            _context = context;
+        }
 
-//        public async Task AgregarAnimalAsync(AnimalSalida animal)
-//        {
-//            _context.AnimalesSalida.Add(animal);
-//            await _context.SaveChangesAsync();
-//        }
+        public async Task AgregarAnimalAsync(AnimalSalida animal)
+        {
+            _context.AnimalesSalida.Add(animal);
+            await _context.SaveChangesAsync();
+        }
 
-//        public async Task BorrarAnimalAsync(int id)
-//        {
-//            var animal = await _context.AnimalesSalida.FindAsync(id);
-//            if (animal is null) return;
-//            _context.AnimalesSalida.Remove(animal);
-//            await _context.SaveChangesAsync();
-//        }
+        public async Task BorrarAnimalAsync(int id)
+        {
+            var animal = await _context.AnimalesSalida.FindAsync(id);
+            if (animal is null) return;
+            _context.AnimalesSalida.Remove(animal);
+            await _context.SaveChangesAsync();
+        }
 
-//        public async Task EditarAnimalAsync(AnimalSalida animal)
-//        {
-//            var animalAEditar = await _context.AnimalesSalida.FindAsync(animal.AnimalId);
+        public async Task EditarAnimalAsync(AnimalSalida animal)
+        {
+            var animalAEditar = await _context.AnimalesSalida.FindAsync(animal.AnimalSalidaId);
 
-//            if (animalAEditar is null) return;
+            if (animalAEditar is null) return;
 
-//            animalAEditar.Tipo = animal.Tipo;
-//            animalAEditar.TipoOtro = animal.TipoOtro;
-//            animalAEditar.Estado = animal.Estado;
-//            animalAEditar.Cantidad = animal.Cantidad;
-//            animalAEditar.Nombre = animal.Nombre;
-//            animalAEditar.Observaciones = animal.Observaciones;
-//            animalAEditar.DamnificadoId = animal.DamnificadoId;
-//            animalAEditar.Damnificado = animal.Damnificado;
+            animalAEditar.Tipo = animal.Tipo;
+            animalAEditar.TipoOtro = animal.TipoOtro;
+            animalAEditar.Estado = animal.Estado;
+            animalAEditar.Cantidad = animal.Cantidad;
+            animalAEditar.Nombre = animal.Nombre;
+            animalAEditar.Observaciones = animal.Observaciones;
 
-//            _context.AnimalesSalida.Update(animalAEditar);
-//            await _context.SaveChangesAsync();
-//        }
+            _context.AnimalesSalida.Update(animalAEditar);
+            await _context.SaveChangesAsync();
+        }
 
-//        public async Task<AnimalSalida?> ObtenerAnimalPorIdAsync(int id)
-//        {
-//            return await _context.AnimalesSalida.FindAsync(id);
-//        }
+        public async Task<AnimalSalida?> ObtenerAnimalPorIdAsync(int id)
+        {
+            return await _context.AnimalesSalida.FindAsync(id);
+        }
 
-//        public async Task<List<AnimalSalida>> ObtenerTodosLosAnimalesAsync()
-//        {
-//            return await _context.AnimalesSalida.Include(a => a.Damnificado).ToListAsync();
-//        }
-//    }
-//}
+        public async Task<List<AnimalSalida>> ObtenerTodosLosAnimalesAsync()
+        {
+            return await _context.AnimalesSalida.ToListAsync();
+        }
+    }
+}
